00638aeaee6af987d7255fd1567d0b9733ef04d3
Daniel Trubač <40642559+dant02@users.noreply.github.com>
2025-03-08 11:49:52 +0100

    add package reference Vlc.DotNet.Core play sound file on start

--- app/Program.cs
+++ app/Program.cs
@@ -1,5 +1,12 @@
+//-----------------------------------------------------------------------
+//  <author>https://github.com/dant02</author>
+//  <date>2025-03-08</date>
+//-----------------------------------------------------------------------
+
 namespace app
 {
+    using Vlc.DotNet.Core;
+
     public class Program
     {
         public static void Main(string[] args)
@@ -9,7 +16,20 @@ namespace app
 
             app.MapGet("/", () => "Hello World!");
 
+            DirectoryInfo info = new(@"C:\Program Files\VideoLAN\VLC");
+
+            var file = new FileInfo(@"d:\music\my.flac");
+
+            var player = new VlcMediaPlayer(info);
+            player.EncounteredError += Player_EncounteredError;
+
+            player.Play(file);
+
             app.Run();
+        }
+
+        private static void Player_EncounteredError(object? sender, VlcMediaPlayerEncounteredErrorEventArgs e)
+        {
         }
     }
 }
